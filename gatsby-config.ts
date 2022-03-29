5bce46bcd6b4d9066273b6df2b1e8f3d67f892fe
yokozawa0701 <d.yoko.0701@gmail.com>
2022-03-29 22:44:42 +0900

    faviconの追加

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -4,12 +4,17 @@ const config: GatsbyConfig = {
   siteMetadata: {
     title: 'Maaji inc.',
     siteUrl: 'https://maaji.co.jp',
-    icon: 'src/images/favicon.ico', 
   },
   plugins: [
     'gatsby-plugin-image',
     'gatsby-plugin-sharp',
     'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        icon: 'src/images/favicon.png',
+      },
+    }
   ],
 }
 
