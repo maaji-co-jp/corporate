2d8b65ee5042fed51785e177b279da0c63587adc
yokozawa0701 <d.yoko.0701@gmail.com>
2022-04-04 23:07:57 +0900

    Heroテキスト追加

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,10 +1,16 @@
 import * as React from 'react'
+import { Box, Container, Typography } from '@mui/material';
 import * as styles from './hero.module.css'
 
 const Hero = () => {
   return(
-    <div className={styles.container}>
-    </div>
+    <Box className={styles.container}>
+      <Container>
+        <Typography className={styles.heroText} variant="h3" component="p">
+          テクノロジーで<br />世界を楽しく
+        </Typography>
+      </Container>
+    </Box>
   )
 }
 
